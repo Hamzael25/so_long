c91e321be85000dd93816a85612a9cd64c737c4a
Hamza El Ouardi <hel-ouar@z3r4p3.42lyon.fr>
2023-01-22 12:47:04 +0100

    suppression des print move dans le terminal

--- ft_move_direction.c
+++ ft_move_direction.c
@@ -6,7 +6,7 @@
 /*   By: hel-ouar <hel-ouar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 16:08:21 by hel-ouar          #+#    #+#             */
-/*   Updated: 2023/01/09 16:08:44 by hel-ouar         ###   ########.fr       */
+/*   Updated: 2023/01/22 12:45:42 by hel-ouar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,26 +16,22 @@ void	ft_move_right(t_mlx *m)
 {
 	m->px += 10;
 	m->move++;
-	ft_printf("%d ", m->move);
 }
 
 void	ft_move_left(t_mlx *m)
 {
 	m->px -= 10;
 	m->move++;
-	ft_printf("%d ", m->move);
 }
 
 void	ft_move_up(t_mlx *m)
 {
 	m->py -= 10;
 	m->move++;
-	ft_printf("%d ", m->move);
 }
 
 void	ft_move_low(t_mlx *m)
 {
 	m->py += 10;
 	m->move++;
-	ft_printf("%d ", m->move);
 }
